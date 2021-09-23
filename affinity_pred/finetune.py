9f1ee49717aff4c2f10c42570e0923b97bf68158
Jens Glaser <glaserj@ornl.gov>
2021-09-23 17:21:09 -0400

    fix left-over line from debugging

--- affinity_pred/finetune.py
+++ affinity_pred/finetune.py
@@ -198,8 +198,7 @@ def main():
         )['train']
 
     # keep a small holdout data set
-    #split_test = data_all.train_test_split(train_size=0.99, seed=0)
-    split_test = data_all.train_test_split(train_size=125,test_size=100, seed=0)
+    split_test = data_all.train_test_split(train_size=0.99, seed=0)
 
     # further split the train set
     f = 0.9
