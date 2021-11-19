6cbee7d81463056edcfa7f13c74cbacb14a921d1
Jens Glaser <glaserj@ornl.gov>
2021-11-19 11:49:46 -0500

    update finetuning dataset

--- affinity_pred/finetune.py
+++ affinity_pred/finetune.py
@@ -185,7 +185,7 @@ def main():
     # split the dataset
     #data_all = load_dataset("jglaser/binding_affinity",split='train')
     data_all = load_dataset('parquet',
-        data_files='/gpfs/alpine/world-shared/bip214/binding_affinity/data/all_ic50.parquet',
+        data_files='/gpfs/alpine/world-shared/bip214/binding_affinity/data/all.parquet',
         features=datasets.Features(
             {
                 "seq": datasets.Value("string"),
