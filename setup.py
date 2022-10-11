b37fbcd54b0c1e1eb1c404ec90eef25802d8fe07
Jens Glaser <glaserj@ornl.gov>
2022-10-11 17:22:54 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup, find_packages
 
 setup(name='affinity_pred',
       packages=['affinity_pred'],
-      version=0.1,
+      version=1.0,
       description='Protein-ligand affinity prediction',
       author='Jens Glaser',
       author_email='glaserj@ornl.gov',
       url='https://github.com/jglaser/affinity_pred',
       license='BSD',
-      keywords=['affinity prediction','transformers','NLP','SMILES','pytorch','deepspeed'],
+      keywords=['affinity prediction','transformers','NLP','SMILES','pytorch'],
+      install_requires=[
+          'transformers',
+          'datasets',
+          'torch',
+      ],
       )
